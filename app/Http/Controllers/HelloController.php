ce8fecfda29aa09aa50fc7cfd2b1d8a10073fb0e
Junya WADA <junya-wada@msen.jp>
2021-02-23 23:23:13 +0900

    Request, Response の使用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,23 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request; // Request を使える状態にしている
+use Illuminate\Http\Response; // Response を使える状態にしている
 
 class HelloController extends Controller
 {
-    public function index($id='null', $pass='null') { // アクションメソッド
-        return <<<EOF
+
+    public function index(Request $request, Response $response) { // アクションメソッド
+        $html = <<<EOF
 <html>
 <head>
 <title>Hello/index</title>
 </head>
 <body>
-<h1>Index</h1>
-<p>これは、 Hello コントローラの index アクションです。</p>
-<p>id:   {$id}</p>
-<p>pass: {$pass}</p>
+<h1>Hello</h1>
+<h3>Request</h3>
+<pre>{$request}</pre>
+<h3>Response</h3>
+<pre>{$response}</pre>
+
 </body>
 </html>
 EOF;
-
+        $response->setContent($html);
+        return $response;
     }
+
+
 }
