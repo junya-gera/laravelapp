9ecdfa5e8499dece88f63408a41682064ff629f9
Junya WADA <junya-wada@msen.jp>
2021-03-06 23:28:36 +0900

    コントローラでフォームリクエストをインポート

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request; // Request を使える状態にしている
 use Illuminate\Http\Response; // Response を使える状態にしている
+use App\Http\Requests\HelloRequest; // 作成したフォームリクエストを使える状態にしている
 
 class HelloController extends Controller
 {
@@ -17,7 +18,8 @@ class HelloController extends Controller
         return view('hello.index', $data);
     }
 
-    public function post(Request $request) {
+    // 渡される引数を Request から HelloRequest に変更することで HelloRequest で作成したバリデーションのルールを適用させる
+    public function post(HelloRequest $request) {
         // テンプレートの指定した name 属性を $request-> するとフォームで送信された値を取得できる
         $msg = $request->msg;
         $data = [
